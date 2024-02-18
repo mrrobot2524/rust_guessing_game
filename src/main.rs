e41576551d73d21f73ecc1a8aa2d317b2caf8b23
mr-robot <alenjon2594@gmail.com>
2024-02-18 16:39:30 +0500

    This is a game, which i make on Rust programming language

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number");
+
+    let  secret_number:u8 = rand::thread_rng().gen_range(1..=101); 
+
+    // println!("Secret number {}", secret_number);
+    loop{
+        println!("Please input your guess.");
+
+        let mut guess:String = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line!");
+
+        println!("You guessed: {}", guess);
+        let guess:u8 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number from 1 to 100!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+            Ordering::Greater => println!("Too big."),
+        }
+    }
+}
